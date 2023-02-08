cbe3dc897a9e0f8b80e774ed2cb40163d2519bcb
Nayek <supratim.nayek@capgemini.com>
2023-02-08 15:17:22 +0530

    Added states for expense form component

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -4,12 +4,23 @@ import "react-datepicker/dist/react-datepicker.css";
 import "./ExpenseForm.css";
 function ExpenseForm() {
   const [startDate, setStartDate] = useState(new Date());
-  const handleClick = (data) => {
-    console.log(data);
+  const [expenseName, setExpenseName] = useState("");
+  const [expenseCost, setExpenseCost] = useState("");
+  const [expenseDescription, setExpenseDescription] = useState("");
+  const handleClick = () => {
+    console.log({
+      date: startDate,
+      name: expenseName,
+      cost: expenseCost,
+      description: expenseDescription,
+    });
   };
 
   return (
-    <form className="form-outer-div">
+    <form
+      className="form-outer-div"
+      onSubmit={(event) => event.preventDefault()}
+    >
       <div className="form-title">
         <h3>Please Submit Your Expense Here</h3>
       </div>
@@ -17,22 +28,36 @@ function ExpenseForm() {
         <div className="input-group">
           <DatePicker
             selected={startDate}
-            onChange={(date) => setStartDate(date)}
+            onChange={(date) => {
+              setStartDate(date);
+            }}
             wrapperClassName="datePicker"
             dateFormat="dd/MM/yyyy"
             className="input"
           />
         </div>
         <div className="input-group">
-          <input type="text" className="input" />
+          <input
+            type="text"
+            className="input"
+            onChange={(event) => setExpenseName(event.target.value)}
+          />
           <label className="user-label">Expense Name</label>
         </div>
         <div className="input-group">
-          <input type="text" className="input" />
+          <input
+            type="text"
+            className="input"
+            onChange={(event) => setExpenseCost(event.target.value)}
+          />
           <label className="user-label">Expense Cost</label>
         </div>
         <div className="input-group">
-          <input type="text" className="input" />
+          <input
+            type="text"
+            className="input"
+            onChange={(event) => setExpenseDescription(event.target.value)}
+          />
           <label className="user-label">Expense Description</label>
         </div>
       </div>
